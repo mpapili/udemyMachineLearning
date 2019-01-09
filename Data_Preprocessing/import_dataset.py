2e2a1bf0dd4f75ce5d06d2201253db76ce91781f
mpapili <mpapili@bloomberg.net>
2019-01-09 18:30:02 -0500

    handling categorical data

--- Data_Preprocessing/import_dataset.py
+++ Data_Preprocessing/import_dataset.py
@@ -11,3 +11,21 @@ X = dataset.iloc[:, :-1].values   # everything but last column
 print(X)
 y = dataset.iloc[:, 3]
 print(y)
+
+# fill in the missing data with mean-values
+from sklearn.preprocessing import Imputer
+imputer = Imputer(missing_values = 'NaN', strategy = 'mean', axis=0)
+imputer = imputer.fit(X[:, 1:3])	# use all rows and columns 1 and 2 (1:3 because upper-limit is excluded)
+X[:, 1:3] = imputer.transform(X[:, 1:3])
+print(X)
+
+# encode categorical data (strings)
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_X = LabelEncoder()
+X[:, 0] = labelencoder_X.fit_transform(X[:, 0])	# all rows, column 0
+onehotencoder = OneHotEncoder(categorical_features = [0])
+X = onehotencoder.fit_transform(X).toarray()
+print(X)
+labelencoder_Y = LabelEncoder()
+y = labelencoder_Y.fit_transform(y)
+print(y)
